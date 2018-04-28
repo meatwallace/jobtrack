9c49769e9d473cc519be5f052e366f422c94bcf0
Geoff Whatley <me@geoffwhatley.com>
2018-04-28 18:07:07 +1000

    refactor(frontend): use async/await and class fields

--- frontend/src/Names/Names.js
+++ frontend/src/Names/Names.js
@@ -1,26 +1,27 @@
-import React, {Component} from 'react';
-
-import TextField from 'material-ui/TextField';
-import ApiService from '../services/ApiService';
-
 import './Names.css';
 
+import TextField from 'material-ui/TextField';
+import React, {Component} from 'react';
+import ApiService from '../services/ApiService';
+
 class Names extends Component {
+  // skipped the constructor and defined these as class fields
+  apiService = new ApiService();
+  state = { userAndJob: 'SomeGuyAndSomeJob' }
 
-  constructor(props) {
-    super(props);
-    this.apiService = new ApiService();
-    this.state = {
-      userAndJob: 'SomeGuyAndSomeJob'
-    };
-  }
+  // when defining prop types on a class component, define them as statics
+  // static propTypes = { ... };
+  // static defaultProps = { ... };
 
-  componentDidMount() {
-    this.apiService
-      .getUserAndJob()
-      .then(response => {
-        this.setState({userAndJob:response});
-      });
+  // converted to async/await - check notes in ApiService
+  async componentDidMount() {
+    const userAndJob = await this.apiService.getUserAndJob();
+
+    // this isn't required at the moment, but depending how complex this component grows, it may.
+    // thought it was worth illustrating that you can use `setState` with a callback.
+    // `setState` actually updates `this.state` asynchronously, so if you ever want to do something like
+    // a counter, this is highly important ex. `this.setState(prev => ({ count: prev.count++ }))`
+    this.setState((prevState) => ({ userAndJob }))
   }
 
   render() {
--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -1,14 +1,27 @@
-class ApiService {
+// lifted this out of the component as it's at the moment it's a constant value
+const url = 'http://192.168.99.100:31000/';
 
-  getUserAndJob(responseCallback) {
-    var url = 'http://192.168.99.100:31000/';
-    console.log("getUserAndJob");
-    return fetch(url)
-    .then(res => {
-      console.log("Got response from getUserAndJob - " );
-      return res.text()
-    })
+class ApiService {
+  // rewrote using async/await. a function prefixed with `async` returns a promise,
+  // and allows usage of `await` to write synchronous LOOKING when working with promises.
+  async getUserAndJob() {
+    const res = await fetch(url);
+
+    return res.text();
   }
+
+  // using regular promise syntax, left for comparison. this is the same as above because
+  // `fetch` returns a promise.
+  // getUserAndJob() {
+  //   return fetch(url).then(res => res.text());
+  // }
+
+  // another example, to illustrate - all 3 of these are interchangeable
+  // getUserAndJob() {
+  //   return new Promise((resolve) => {
+  //     fetch(url).then(res => resolve(res.text()))
+  //   });
+  // }
 
   getUser() {
     return "Barry White";
